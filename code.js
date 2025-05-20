4e0df5753575ecb6d7180c2e8dd7aee789a7724d
GeneJustin <justinz011206@gmail.com>
2025-05-20 14:26:21 +0700

    Update code.js

--- code.js
+++ code.js
@@ -1,4 +1,5 @@
 function sub() {
+    let tulis = document.getElementById("result-section");
     const age = Number(document.getElementById("age").value);
     const tb = Number(document.getElementById("tb").value);
     const res = document.getElementById("stat");
@@ -15,10 +16,21 @@ function sub() {
         kaloriPerKg = 90;
     } else {
         needs.textContent = "Usia di luar rentang balita.";
+        res.textContent = "-";
         return;
     }
 
+    
+    tulis.classList.remove("hidden");
+    tulis.classList.add("result-section");
+
     const kebutuhanKalori = Math.round(bbi * kaloriPerKg);
+
+    if(isNaN(tb) || isNaN(bb)){
+        res.textContent = "Input tidak lengkap";
+        needs.textContent = "Silahkan Masukkan Ulang";
+        return;
+    }
 
     res.textContent = `Berat Badan Ideal Anak Balita usia ${age} tahun, dengan tinggi ${tb} cm adalah ${bbi} kg.`;
     needs.textContent = `Kebutuhan kalori harian ideal untuk anak anda : ${kebutuhanKalori} kkal.`;
